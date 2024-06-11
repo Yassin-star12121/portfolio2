b159f87332f13789e19e19d578eeff7827190580
Yassin-star12121 <web.domain.web1@gmail.com>
2024-06-11 07:34:42 +0300

    Add files via upload

---
+++ Navbar/2/js/script.js
@@ -0,0 +1,45 @@
+//Javascript programming
+//javascript with text stylesheet
+//Function with html and css
+
+function myFunction() {
+  window.alert("The next page is Google.com")
+}
+
+function codeFunction() {
+  window.alert("The next page is codepen.io");
+}
+
+function schoolsFunction() {
+  window.alert("The next page is w3schools.com")
+}
+
+function youtubeFunction() {
+  window.alert("The next page is youtube.com")
+}
+
+function facebookFunction() {
+  window.alert("The next page is facebook.com")
+}
+
+function coffeecupFunction() {
+  window.alert("The next page is coffee world");
+}
+
+function wifirootFunction() {
+  window.alert("The next page is cyber wifi")
+}
+
+function javascriptFunction() {
+  window.alert("Learn JavaScript to JavaScript page");
+}
+
+function show() {
+  document.getElementById("fa-search").classList.toggle("active");
+  document.getElementById("fa-wifi").classList.toggle("active2");
+  document.getElementById("fa-bars").classList.toggle("active3");
+  document.getElementById("fa-laptop").classList.toggle("active4");
+  document.getElementById("fa-power-off").classList.toggle("active5");
+  document.getElementById("fa-code").classList.toggle("active6");
+  document.getElementById("fa-coffee").classList.toggle("active7");
+}
